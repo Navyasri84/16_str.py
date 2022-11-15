e847bcfc1be1ef962cf68eb9c9a890af00700b68
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-11-15 17:38:50 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,47 @@
+# a=3
+# print("The value of a is",a) #The value of a is 3
+# print("The value of a is ",a) #The value of a is  3
+
+# b=""
+# print(b,type(b)) # str
+
+# c="    "
+# print(c.isspace()) #True
+
+
+# d=5
+# print(type(d)) #int
+# print(int(d)) #5
+# print(float(d)) #5.0
+# print(str(d)) #5
+
+# b=7.8 
+# print(type(b)) #float
+# print(float(b)) #7.8
+# print(int(b)) #7
+# print(str(b)) #7.8
+
+# d='DELL'
+# print("One of the Laptop Brand is",d) #One of the Laptop Brand is DELL
+# print(''+d) #DELL
+# print(' '+d) # DELL
+# print("One of the Laptop Brand is"+d) #One of the Laptop Brand isDELL
+# print("One of the Laptop Brand is"+' '+d) #One of the Laptop Brand is DELL
+
+# d="12"
+# print(d,type(d)) #12 str
+# print(int(d)) #12
+# print(float(d)) #12.0
+
+
+# print(int(5.6)) #5
+# print(float(3)) #3.0
+# print(int("python")) #ValueError
+# print(float("program")) #ValueError
+
+# e='67.45'
+# print(e,type(e)) #67.45 sr
+# g=float(e) 
+# print(g) #67.45
+# print(int(e)) #ValueError
+# print(int(g)) #67 
